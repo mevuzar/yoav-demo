7e3ad797aceabc22467fa54e412c62613b2ed905
yoyomobli <yoav.sadeh@gmail.com>
2016-10-10 10:13:41 +0300

    added store products and users services

---
+++ src/main/scala-2.11/com/hamlazot/domain/contract/server/StoresService.scala
@@ -0,0 +1,10 @@
+package com.hamlazot.domain.contract.server
+
+import com.hamlazot.{CommonTerms, CommonOperations}
+
+/**
+ * Created by Owner on 9/30/2016.
+ */
+trait StoresService extends CommonOperations with CommonTerms{
+
+}
