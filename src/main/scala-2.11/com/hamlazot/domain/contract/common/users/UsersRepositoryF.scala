efd44ee838f8793d4e92e8fa3d0f0b033eb522f4
yoyomobli <yoav.sadeh@gmail.com>
2016-10-30 12:37:10 +0200

    added repository monad implemented create user operation

---
+++ src/main/scala-2.11/com/hamlazot/domain/contract/common/users/UsersRepositoryF.scala
@@ -0,0 +1,35 @@
+package com.hamlazot.domain.contract.common.users
+
+import java.util.UUID
+
+import com.hamlazot.DataDSL.{DataCall, DataOperations}
+import com.hamlazot._
+
+/**
+ * Created by Owner on 10/30/2016.
+ */
+trait UsersRepositoryF extends UserAggregate {
+
+  object DSL {
+
+    //type FreeCall[A] = Free[DataCall, A]
+
+    sealed trait UsersDataCall[+A] extends DataCall[A]
+
+    final case class UserQuery(id: UUID) extends UsersDataCall[FutureStringOr[User]]
+
+    final case class StoreUser(user: User) extends UsersDataCall[FutureStringOr[UUID]]
+
+  }
+
+  private[domain] object UsersDataOperations extends DataOperations {
+
+    import DSL._
+
+    def query(id: UUID) = dataOperation(UserQuery(id))
+
+    def store[A](user: User) = dataOperation(StoreUser(user))
+
+  }
+
+}
--- src/main/scala-2.11/com/hamlazot/domain/impl/common/users/UsersProductionService.scala
+++ src/main/scala-2.11/com/hamlazot/domain/impl/common/users/UsersProductionService.scala
@@ -4,9 +4,10 @@ package impl.common.users
 
 import java.util.UUID
 import DataDSL.DataStoreRequest
+import com.typesafe.scalalogging.LazyLogging
 import impl.common.users.UsersModel.UserCreationDetails
-import contract.common.users.UsersService
-import impl.common.accounts.AccountModel.UserToken
+import contract.common.users.{UsersService, UsersRepositoryF}
+
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Try
@@ -15,15 +16,40 @@ import scalaz.{Id, ~>}
 /**
  * @author yoav @since 10/11/16.
  */
-trait UsersProductionService extends UsersService with UserAggregate {
+trait UsersProductionService extends UsersService  with UserAggregate with LazyLogging{
   protected val dbDriver: (DataStoreRequest ~> Id.Id)
   protected val dbLogger: (DataStoreRequest ~> Id.Id)
   protected implicit val executionContext: ExecutionContext
-
+  protected implicit val repository: UsersRepositoryF with UserAggregate
+  import repository.UsersDataOperations._
+//
   override type Operation[A, B] = A => Future[B]
 
-  override def createUser: Operation[UserCreationDetails, UUID] = { credentials=>
-    Future(UUID.randomUUID())
+  override def createUser: Operation[UserCreationDetails, UUID] = { request=>
+    Future{
+      val user = UsersModel.User(UUID.randomUUID(), request.name, request.familyName, request.trustees)
+
+      user.id
+    }
+  }
+
+  protected def storeUser(user: User): Future[User] = {
+    val stored = store(user)
+    val dbLog = runFC(stored)(dbLogger).left.get
+    dbLog.map(log => {
+
+      logger.info(log)
+    })
+
+    val dbResult = runFC(stored)(dbDriver).right.get
+    val res = dbResult.map(x => user)
+    res
+  }
+
+  protected def queryAccount(userId: UUID): Future[User] = {
+    val theQuery = query(userId)
+    val dbResult = runFC(theQuery)(dbDriver).right.get
+    dbResult
   }
 }
 
